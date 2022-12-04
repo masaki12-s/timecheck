ea342696c25a3171cca0a3a9d3172da4d0f10148
masaki12-s <asam.archer1112@yahoo.ne.jp>
2022-12-04 19:58:42 +0900

    del init

--- tests/timecheck_test.py
+++ tests/timecheck_test.py
@@ -4,29 +4,34 @@ import unittest
 from speciallecture.Timecheck import Timecheck
 
 class TestTimecheck(unittest.TestCase):
-    def __init__(self) -> None:
-        self.timecheck = Timecheck()
-        
     def test_read1(self):
-        self.assertEqual("OK", self.timecheck.check(0, 0, 22))
+        timecheck = Timecheck()
+        self.assertEqual("OK", timecheck.check(0, 0, 22))
 
     def test_read2(self):
-        self.assertEqual("NG", self.timecheck.check(22, 0, 22))
+        timecheck = Timecheck()
+        self.assertEqual("NG", timecheck.check(22, 0, 22))
 
     def test_read3(self):
-        self.assertEqual("NG", self.timecheck.check(1, 2, 22))
+        timecheck = Timecheck()
+        self.assertEqual("NG", timecheck.check(1, 2, 22))
 
     def test_read4(self):
-        self.assertEqual("NG", self.timecheck.check(23, 2, 22))
+        timecheck = Timecheck()
+        self.assertEqual("NG", timecheck.check(23, 2, 22))
     
     def test_read5(self):
-        self.assertEqual("OK", self.timecheck.check(0, 22, 5))
+        timecheck = Timecheck()
+        self.assertEqual("OK", timecheck.check(0, 22, 5))
     
     def test_read6(self):
-        self.assertEqual("NG", self.timecheck.check(5, 22, 5))
+        timecheck = Timecheck()
+        self.assertEqual("NG", timecheck.check(5, 22, 5))
     
     def test_read7(self):
-        self.assertEqual("OK", self.timecheck.check(22, 22, 5))
+        timecheck = Timecheck()
+        self.assertEqual("OK", timecheck.check(22, 22, 5))
     
     def test_read8(self):
-        self.assertEqual("NG", self.timecheck.check(23, 22, 5))
+        timecheck = Timecheck()
+        self.assertEqual("NG", timecheck.check(23, 22, 5))
